698d8a7e74797729f4f24dc6b3e535dc3b33d2ca
Tuan Le <lenguyenanhtuan95@gmail.com>
2018-09-21 21:55:37 +0700

    Clean up before construct code

--- WeaSIGABRT/WeaSIGABRT/ViewController.swift
+++ WeaSIGABRT/WeaSIGABRT/ViewController.swift
@@ -1,10 +1,17 @@
-//
-//  ViewController.swift
-//  WeaSIGABRT
-//
-//  Created by Tuan Le on 9/21/18.
-//  Copyright © 2018 Tuan Le. All rights reserved.
-//
+/*
+ RMIT University Vietnam
+ Course: COSC2659 iOS Development
+ Semester: 2018B
+ Assessment: Project
+ Author:
+ -   Ngo Vu Nguyen (s3480522)
+ -   Le Pham Ngoc Hoai (s3636085)
+ -   Le Nguyen Anh Tuan (s3574983)
+ -   Mai Pham Quang Huy (s3618861)
+ ID: s3480522,s3636085, s3574983, s3618861
+ Created date: 18/9/2018
+ Acknowledgment:
+*/
 
 import UIKit
 
