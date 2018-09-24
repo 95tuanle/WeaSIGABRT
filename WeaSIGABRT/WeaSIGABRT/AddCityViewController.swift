74750284b99a84cd966236c47d11866ed67898c6
Tuan Le <lenguyenanhtuan95@gmail.com>
2018-09-24 07:03:08 +0700

    Add City Completed (But needs to be improved)

--- WeaSIGABRT/WeaSIGABRT/AddCityViewController.swift
+++ WeaSIGABRT/WeaSIGABRT/AddCityViewController.swift
@@ -1,24 +1,27 @@
-//
-//  AddCityViewController.swift
-//  WeaSIGABRT
-//
-//  Created by Tuan Le on 9/24/18.
-//  Copyright © 2018 Tuan Le. All rights reserved.
-//
+/*
+ RMIT University Vietnam
+ Course: COSC2659 iOS Development
+ Semester: 2018B
+ Assessment: Project
+ Author:
+ -   Ngo Vu Nguyen (s3480522)
+ -   Le Pham Ngoc Hoai (s3636085)
+ -   Le Nguyen Anh Tuan (s3574983)
+ -   Mai Pham Quang Huy (s3618861)
+ ID: s3480522,s3636085, s3574983, s3618861
+ Created date: 18/9/2018
+ Acknowledgment:
+*/
 
 import UIKit
 import MapKit
+import CoreData
 
 class AddCityViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate, MKLocalSearchCompleterDelegate {
     @IBOutlet weak var searchCity: UISearchBar!
-    
     @IBOutlet weak var resultTable: UITableView!
     var results:[String] = []
     var internalResults:[MKLocalSearchCompletion] = []
-    var temp1:String = ""
-    var temp2:String = ""
-    var temp3:[NSValue] = []
-    var temp4:[NSValue] = []
     
     lazy var searchCompleter: MKLocalSearchCompleter = {
         let searchCompleter = MKLocalSearchCompleter()
@@ -50,7 +53,20 @@ class AddCityViewController: UIViewController, UITableViewDataSource, UITableVie
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        self.performSegue(withIdentifier: "", sender: nil)
+//        let searchRequest = MKLocalSearch.Request(completion: internalResults[indexPath.row])
+//        let search = MKLocalSearch(request: searchRequest)
+//        search.start { (response, error) in
+        MKLocalSearch(request: MKLocalSearch.Request(completion: internalResults[indexPath.row])).start { (response, error) in
+            let newCity = SupportFunctions.createNewCity()
+            let mapItem = response?.mapItems[0]
+            let coordinate = mapItem?.placemark.coordinate
+            newCity.name = mapItem?.name
+            newCity.lat = (coordinate?.latitude)!
+            newCity.long = (coordinate?.longitude)!
+            newCity.timezone = mapItem?.timeZone
+            SupportFunctions.saveContext()
+            self.performSegue(withIdentifier: "Added", sender: nil)
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
