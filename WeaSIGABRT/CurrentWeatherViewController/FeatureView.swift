e34d6132980c71fc8e5ddf3e29cdae5e2a0822cf
Tuan Le <lenguyenanhtuan95@gmail.com>
2018-09-28 00:06:17 +0700

    Fix reference + cleaning

--- WeaSIGABRT/CurrentWeatherViewController/FeatureView.swift
+++ WeaSIGABRT/CurrentWeatherViewController/FeatureView.swift
@@ -11,21 +11,22 @@
  ID: s3480522,s3636085, s3574983, s3618861
  Created date: 18/9/2018
  Acknowledgment:
-    https://developer.apple.com/documentation/foundation/date
-    https://developer.apple.com/documentation/foundation/dates_and_times
-    https://developer.apple.com/documentation/uikit/uicollectionview
-    https://developer.apple.com/documentation/foundation/timezone
-    https://developer.apple.com/library/archive/documentation/UserExperience/Conceptual/AutolayoutPG/ProgrammaticallyCreatingConstraints.html
-    https://github.com/Alamofire/Alamofire
-    https://github.com/loopwxservices/WXKDarkSky
-    https://github.com/drtimcooper/LatLongToTimezone
-    https://github.com/romansorochak/Blur
-    https://medium.com/@brianclouser/swift-3-creating-a-custom-view-from-a-xib-ecdfe5b3a960
-    https://stackoverflow.com/questions/52460351/scrollview-in-xib-custom-view-to-fit-with-multiple-width
-    https://stackoverflow.com/questions/52462744/swift-2-for-loop-with-asynchronous-network-request-continue-looping-after-the-re
-    https://stackoverflow.com/questions/36419107/passing-data-between-uipageviewcontroller-child-views-with-swift
-    https://stackoverflow.com/questions/47494222/getting-the-city-country-list-in-ios-time-zone
-    https://stackoverflow.com/questions/46869394/reverse-geocoding-in-swift-4
+ https://developer.apple.com/documentation/foundation/date
+ https://developer.apple.com/documentation/foundation/dates_and_times
+ https://developer.apple.com/documentation/uikit/uicollectionview
+ https://developer.apple.com/documentation/foundation/timezone
+ https://developer.apple.com/library/archive/documentation/UserExperience/Conceptual/AutolayoutPG/ProgrammaticallyCreatingConstraints.html
+ https://developer.apple.com/library/archive/documentation/UserExperience/Conceptual/AutolayoutPG/WorkingwithConstraintsinInterfaceBuidler.html
+ https://github.com/Alamofire/Alamofire
+ https://github.com/loopwxservices/WXKDarkSky
+ https://github.com/drtimcooper/LatLongToTimezone
+ https://github.com/romansorochak/Blur
+ https://medium.com/@brianclouser/swift-3-creating-a-custom-view-from-a-xib-ecdfe5b3a960
+ https://stackoverflow.com/questions/52460351/scrollview-in-xib-custom-view-to-fit-with-multiple-width
+ https://stackoverflow.com/questions/52462744/swift-2-for-loop-with-asynchronous-network-request-continue-looping-after-the-re
+ https://stackoverflow.com/questions/36419107/passing-data-between-uipageviewcontroller-child-views-with-swift
+ https://stackoverflow.com/questions/47494222/getting-the-city-country-list-in-ios-time-zone
+ https://stackoverflow.com/questions/46869394/reverse-geocoding-in-swift-4
  */
 
 import UIKit
